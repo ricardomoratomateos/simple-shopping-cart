e0497eb504fa3c2f5372d817f66ab5f47615801a
ricardomoratomateos <ricardomoratomateos@outlook.com>
2019-10-03 18:52:49 +0200

    improve creation of product test

--- tests/Unit/Domain/Product/ProductTest.php
+++ tests/Unit/Domain/Product/ProductTest.php
@@ -6,10 +6,47 @@ use Uvinum\Domain\Product\Product;
 
 class ProductTest extends TestCase
 {
-    public function testCreateProduct(): void
+    public function createProductProvider(): array
     {
-        $product = new Product();
+        return [
+            'with-values' => [
+                'id' => 1,
+                'price' => 10,
+                'isInOffer' => true,
+                'minOfOfferUnities' => 3,
+                'priceInOffer' => 9,
+            ],
+            'without-values' => [
+                'id' => null,
+                'price' => null,
+                'isInOffer' => null,
+                'minOfOfferUnities' => null,
+                'priceInOffer' => null,
+            ]
+        ];
+    }
+
+    /** @dataProvider createProductProvider */
+    public function testCreateProduct(
+        $id,
+        $price,
+        $isInOffer,
+        $minOfOfferUnities,
+        $priceInOffer
+    ): void {
+        $product = new Product(
+            $id,
+            $price,
+            $isInOffer,
+            $minOfOfferUnities,
+            $priceInOffer
+        );
 
         $this->assertInstanceOf(Product::class, $product);
+        $this->assertSame($id, $product->getId());
+        $this->assertSame($price, $product->getPrice());
+        $this->assertSame($isInOffer, $product->isInOffer());
+        $this->assertSame($minOfOfferUnities, $product->getMinOfOfferUnities());
+        $this->assertSame($priceInOffer, $product->getPriceInOffer());
     }
 }
